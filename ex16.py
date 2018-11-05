07d88d15c19e1f8b5b8b15fd53df45fd5f0938a2
Nova-Xue <44712321+Nova-Xue@users.noreply.github.com>
2018-11-04 23:24:14 -0500

    Add files via upload

---
+++ ex16.py
@@ -0,0 +1,37 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We are going to erase {filename}.")
+print("If you don`t want that, hit CTRL-C (^C).")
+print("If you do want taht, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+#target = open(filename)
+#will result in File not open for writing
+#truncate() is a write function
+print("Truncating the file.   Goodbye!")
+target.truncate()
+
+print("Now I`m going to ask for three lines.")
+
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("I`m going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally , we close it.")
+target.close()
+#should close all file
+#if not leak file and will crash
